9984dd6509404fa48d9c3aa8131fc3941c3f048c
Andrew Cox <andrew@ifsight.com>
2022-11-11 09:04:45 -0500

    Only commit the necessary node_modules/breakpoint-sass folder.

--- web/themes/custom/sand/node_modules/@ronilaukkarinen/stylelint-a11y/jest.setup.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const stylelint = require('stylelint');
-const getTestRule = require('jest-preset-stylelint/getTestRule');
-
-global.testRule = getTestRule(stylelint, { plugins: ['./src'] });
