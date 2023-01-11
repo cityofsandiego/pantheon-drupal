bed1d677462526f6fb3a7fb56b7c5e6cb93dbc5e
Javiera Garcia <javiera@ifsight.com>
2023-01-11 12:30:54 -0500

    Add text style, display mode and border bottom

--- web/modules/custom/if_components/src/EventSubscriber/Preprocess/Paragraph/SectionsOutreach2.php
+++ web/modules/custom/if_components/src/EventSubscriber/Preprocess/Paragraph/SectionsOutreach2.php
@@ -21,11 +21,12 @@ final class SectionsOutreach2 implements EventSubscriberInterface {
   protected $entityTypeManager;
 
   /**
-   * Text classes to display.
+   * Text style.
    *
    * @var array
    */
-  protected $textClasses = [];
+  protected $textStyle = [];
+
 
   /**
    * Background image styles.
@@ -50,7 +51,7 @@ final class SectionsOutreach2 implements EventSubscriberInterface {
     ];
   }
 
-    /**
+  /**
    * Implements hook_preprocess_paragraph() for the section outreach 2.
    *
    *
@@ -62,50 +63,84 @@ final class SectionsOutreach2 implements EventSubscriberInterface {
 
     $paragraph = $variables->getParagraph();
 
-    // Reset background Style since they seem to stick when there are multiple
+    // Reset styles since they seem to stick when there are multiple
     // sections paragraphs on the page.
     $this->bgStyle = [];
-    //dump($paragraph);
+    $this->textStyle = [];
 
-    if (!$paragraph->field_image->isEmpty()) {
-      // Preprocess image to get URL
-      $field_image = $paragraph->field_image->getValue();
 
-      // $image = $this->entityTypeManager->getStorage('media')
-      // ->load( $paragraph->get('field_image')->getValue()[0]['target_id']);
-      $url = 'http://san-diego-custom-code.lndo.site/sites/default/files/styles/large/public/downtown-skyline-.jpg?itok=QqEnXL6h';
-      // Use background_image
-      $this->bgStyle = [
+    // Set background image or background color
+      if (!$paragraph->field_image->isEmpty()) {
+        // Preprocess image to get URL
+        $field_image = $paragraph->field_image->getValue();
 
-        'size' => 'background-size: 100%',
-        'position' => 'background-position: 50% 50%',
-        'min-height' => 'min-height: 300px',
-        'image' => 'background-image: url(' . $url . ')',
-      ];
-
-    } else {
-      //Use background_color
-      if ($paragraph->field_bg_color){
+        // $image = $this->entityTypeManager->getStorage('media')
+        // ->load( $paragraph->get('field_image')->getValue()[0]['target_id']);
+        $url = 'http://san-diego-custom-code.lndo.site/sites/default/files/styles/large/public/downtown-skyline-.jpg?itok=QqEnXL6h';
+        // Use background_image
         $this->bgStyle = [
-          'bg-color' => 'background-color:' . $paragraph->field_bg_color->value,
+          'size' => 'background-size: 100%',
+          'position' => 'background-position: 50% 50%',
+          'min-height' => 'min-height: 300px',
+          'image' => 'background-image: url(' . $url . ')',
         ];
       } else {
-        $this->bgStyle = [
-          'bg-color' => 'background-color: #FFF',
-        ];
-      }
-    };
+        //Use background_color
+        if ($paragraph->field_bg_color){
+          $this->bgStyle = [
+            'bg-color' => 'background-color:' . $paragraph->field_bg_color->value,
+          ];
+        } else {
+          $this->bgStyle = [
+            'bg-color' => 'background-color: #FFF',
+          ];
+        }
+      };
 
-    if ($paragraph->field_centered){ 
-      $this->textClasses[] = ['text-center'];
+
+    // Hide on desktop or mobile fields
+      if ($paragraph->field_hide_on_desktop->value) {
+        $variables->set('hide_on_desktop', 'hide-on-desktop');
+      } 
+
+      if ($paragraph->field_hide_on_mobile->value) {
+        $variables->set('hide_on_mobile', 'hide-on-mobile');
+      } 
+
+    // Set text styles (field_centered, field_no_drop_shadow, field_no_styling)
+      if ($paragraph->field_centered->value) {
+        $this->textStyle[] = 'text-center';
+      } 
+      if ($paragraph->field_no_drop_shadow->value) {
+        $this->textStyle[] = 'no-shadow';
+      }
+      if ($paragraph->field_no_styling->value) {
+        $this->textStyle[] = 'no-styling';
+      }
+
+    // Set border bottom (field_bottom_border)
+    if (!$paragraph->field_bottom_border->isEmpty()) {
+      
+      $field_bottom_border = $paragraph->field_bottom_border->value;
+      switch ($field_bottom_border) {
+        case '0':
+          $border = 'border-bottom-none';
+          break;
+  
+        case '1':
+          $border = 'border-bottom-thin';
+          break;
+  
+        case '2':
+          $border = 'border-bottom-thick';
+          break;
+      };
+      $variables->set('border_bottom', $border);
     }
 
-    // $bgStyleString = implode(";", $this->bgStyle);
-    // dump($string);
-
-    // Send array of contact info to the paragraph display.
+    // Send array of style variables to the paragraph display.
     $variables->set('bg_style', implode(";", $this->bgStyle));
     $variables->set('bg_attributes', $this->bgAttributes);
-    $variables->set('text_classes', $this->textClasses);
+    $variables->set('text_classes', implode(" ", $this->textStyle));
   }
 }
