1e89585128dc36403dfc50265da63db8dab43da1
Glen Shewchuck <gshewchuck@sandiego.gov>
2023-01-18 15:52:16 -0800

    updated modules sand_weather

---
+++ web/modules/custom/sand_weather/src/Plugin/Block/SanDiegoWeatherBlock.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\sand_weather\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a san diego weather block.
+ *
+ * @Block(
+ *   id = "sand_weather",
+ *   admin_label = @Translation("San Diego Weather"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class SanDiegoWeatherBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    
+    $weather_text = \Drupal::state()->get('sand_weather.text');
+    $weather_icon = \Drupal::state()->get('sand_weather.icon');
+    $weather_temp = \Drupal::state()->get('sand_weather.temp');
+
+    if (empty($weather_text) || empty($weather_icon) || empty($weather_temp)) {
+      return [
+        '#theme' => 'sand_weather',
+        '#weather-text' => "",
+        '#weather-icon' => "",
+        '#weather-temp' => "",
+      ];
+    } else {
+      return [
+        '#theme' => 'sand_weather',
+        '#text' => $weather_text,
+        '#icon' => $weather_icon,
+        '#temp' => $weather_temp,
+      ];
+    }
+
+//    $build['content'] = [
+//      '#markup' => $weather,
+//    ];
+  }
+}
