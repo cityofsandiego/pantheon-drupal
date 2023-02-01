d06a365c45c30a1d3a99a01fe84c23be7e5226bc
Daniel Boone Brabon <dbrabon@sandiego.gov>
2023-02-01 12:45:27 -0800

    hero dept id in js

--- web/modules/custom/sand_hero/js/sand_hero_bg_image.js
+++ web/modules/custom/sand_hero/js/sand_hero_bg_image.js
@@ -8,8 +8,9 @@
   Drupal.behaviors.heroBgImage = {
     attach: function (context, settings) {
       var myVariable = drupalSettings.sandHero.heroNodeIDs;
-      console.log(myVariable);
+      console.log("outside Once");
       $('#hero-bg-image', context).once('heroBgImage').each(function () {
+        console.log(myVariable);
         $(this).append("<script>console.log('Hello from Hero Rotation!')</script>");
       });
     }
