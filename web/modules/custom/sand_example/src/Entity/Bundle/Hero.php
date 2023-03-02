595e65f5a57fe863e273cbfed5d20bc68cc1e5e2
Glen Shewchuck <gshewchuck@sandiego.gov>
2023-03-02 09:09:49 -0800

    add more to custom examples module

--- web/modules/custom/sand_example/src/Entity/Bundle/Hero.php
+++ web/modules/custom/sand_example/src/Entity/Bundle/Hero.php
@@ -3,13 +3,21 @@
 namespace Drupal\sand_example\Entity\Bundle;
 
 use Drupal\node\Entity\Node;
+use Drupal\Core\Entity\EntityStorageInterface;
 
-class Hero extends Node implements DepartmentInterface {
+class Hero extends Node implements DepartmentInterface, HeroInterface {
 
   use DepartmentTraits;
 
   public function getTimeOfDay(): string {
     return $this->get('field_hero_time_of_day')->value;
   }
+
+  public function postSave(EntityStorageInterface $storage, $update = TRUE) {
+    if ($update == FALSE) {
+      \Drupal::messenger()->addStatus(t('Thank you for adding the new hero @title.', ['@title' => $this->toLink()->toString()]));
+    }
+    parent::postSave($storage, $update);
+  } 
 
 }
