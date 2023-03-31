3523c50e6215bef063b576b1eea5a979b90bf8df
Javiera Garcia <javiera@ifsight.com>
2023-03-31 16:09:56 -0400

    Update Outreach style and add modernizr

--- web/themes/custom/sand/js/outreach2a.js
+++ web/themes/custom/sand/js/outreach2a.js
@@ -1,12 +1,14 @@
 jQuery(function(){
 
 if (!Modernizr.touch) {
+  console.log('if outreach2a')
     jQuery.stellar({
       horizontalScrolling: false,
       //responsive: true
       //verticalOffset: 50
     });
 } else {
+  console.log('else outreach2a')
   jQuery(".stellar-window").css({"background-attachment": "scroll", "background-position": "50% 50%"});
 }
 });
