588139cf9b59dda4ff81516254f3390b1084dc3b
Glen Shewchuck <gshewchuck@sandiego.gov>
2023-04-10 09:17:29 -0700

    make sure devel config is not exported to production

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,3 +32,6 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+// Added by City of San Diego
+$settings['config_exclude_modules'] = ['devel'];
