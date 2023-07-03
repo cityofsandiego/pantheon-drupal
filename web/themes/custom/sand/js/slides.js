f3f32c06e300ab128013d03b0b967beae81d01d9
Ron Vazquez <rvazquez@sandiego.gov>
2023-07-03 11:30:40 -0700

    installed flexslider module and made related css and twig changes

--- web/themes/custom/sand/js/slides.js
+++ web/themes/custom/sand/js/slides.js
@@ -2,8 +2,6 @@
  * @file
  * A JavaScript file for the theme.
  *
- * In order for this JavaScript to be loaded on pages, see the instructions in
- * the README.txt next to this file.
  */
 
 // JavaScript should be made compatible with libraries other than jQuery by
@@ -16,41 +14,16 @@
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
   Drupal.behaviors.slides = {
     attach: function (context, settings) {
-      $('#hero__slides--primary').flexslider({
-        controlNav: false,
-        customDirectionNav: $(".custom-navigation--primary a")
-      });
-      $('#hero__slides--secondary').flexslider({
-          controlNav: false,
-          customDirectionNav: $(".custom-navigation--secondary a")
-      });
-      $('.flexslider--default').flexslider({
-        animation: "slide",
-        slideshow: false,
+      $('.flexslider').flexslider({
         start: function(slider) {
-          var count = $('.total-slides').text(slider.count);
-        },
-        after: function(slider) {
-          $('.current-slide').text(slider.currentSlide+1);
+          var $slides = jQuery("div#flexslider-home .slides li")
+          //console.log($slides);
+          if ($slides.length > 1) {
+            $('.hero__content .view-homepage-slide').attr('style', 'padding-bottom: 3em');
+          }
+          //console.log("flex started");
+          $('div.hero__content').fadeIn('slow');
         }
-      });
-      var counter = '<li class="flex-counter"><span class="current-slide">1</span> / <span class="total-slides"></span></li>';
-      $('.flexslider .flex-control-nav').html( counter );
-      var index = $('.flexslider li:has(.flex-active)').index('.flex-control-nav li')+1;
-      var total = $('.flexslider .flex-control-nav li').length;
-      $('.flexslider--thumbnails').flexslider({
-        animation: "slide",
-        slideshow: false,
-        controlNav: false,
-        sync: ".flexslider--thumbnails-nav"
-      });
-      $('.flexslider--thumbnails-nav').flexslider({
-        animation: "slide",
-        itemWidth: 150,
-        itemMargin: 5,
-        slideshow: false,
-        controlNav: false,
-        asNavFor: ".flexslider--thumbnails"
       });
       // Mobile only slider
       if ( $(window).width() < 480 ) {
@@ -70,16 +43,3 @@
   // and, in this file's first line of JS, change function (Drupal) to
   // (Drupal, $)
 })(Drupal, jQuery);
-
-jQuery(function () {
-  if (jQuery('body').hasClass('front')) {
-    var $slides = jQuery("div#hero__slides--primary .slides li")
-    console.log($slides);
-    if ($slides.length > 1) {
-      jQuery("div.hero__content").addClass("multi");
-      jQuery("div.custom-navigation").removeClass("hidden").fadeIn("slow");
-    }
-    jQuery("div#hero__slides--primary").removeClass("hidden");
-    jQuery("div#get_it_done").removeClass("hidden");
-  }
-});
