4ad8eb78c556791293a5e0be7af8cfbd122921cd
Glen Shewchuck <glenshewchuck@gmail.com>
2023-09-13 09:50:18 -0700

    add modules for migration and move them to dev-req

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -39,5 +39,5 @@ if(file_exists($redirect_file)) {
 }
 
 // Added by City of San Diego
-$settings['config_exclude_modules'] = ['devel', 'webprofiler', 'examples', 'config_single_export', 'restui', 'views_bulk_edit', 'entity_count' ];
+$settings['config_exclude_modules'] = ['devel', 'webprofiler', 'examples', 'config_single_export', 'restui', 'views_bulk_edit', 'entity_count', 'media_migration', 'bean_migrate', 'migrate_sandbox', 'yaml_editor' ];
 
