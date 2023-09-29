01d8570f413b2f6bfe4d4db563c545d746b146a0
Daniel Boone Brabon <dbrabon@sandiego.gov>
2023-09-29 10:23:12 -0700

    Adding SimpleSaml cert files.

--- private/simplesamlphp/config/authsources.php
+++ private/simplesamlphp/config/authsources.php
@@ -26,8 +26,8 @@ $config = [
 
         'NameIDPolicy' => NULL,
 
-       'privatekey' => 'private/simplesamlphp/cert/saml.pem',
-       'certificate' => 'private/simplesamlphp/cert/saml.crt',
+       'privatekey' => 'saml.pem',
+       'certificate' => 'saml.crt',
 
         // Enforce signing of redirections, using our certificate.
         'sign.logout' => TRUE,
