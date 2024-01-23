3c6f267a0f1fb9c4f9428512ea9e34b91b5433c1
Glen Shewchuck <glenshewchuck@gmail.com>
2024-01-23 13:05:07 -0800

    feeds: take out unlock from feed_import.php and cleanup

--- web/scripts/feed_import.php
+++ web/scripts/feed_import.php
@@ -8,28 +8,29 @@ use Drupal\feeds\Entity\Feed;
 use Drupal\feeds\FeedInterface;
 
 if (empty($extra[0])) {
-  $message = "Called feed import script with no feed id number";
-  \Drupal::logger('my_module')->error($message);
+  \Drupal::logger('feed_import.php')->error("Called feed import script with no feed id number");
   return FALSE;
 } else {
   $id = $extra[0];
 }
 
 if (!is_numeric($id)) {
-  $message = "Called feed import script with a non numeric feed id number";
-  \Drupal::logger('my_module')->error($message);
+  \Drupal::logger('feed_import.php')->error( "Called feed import script with a non numeric feed id number of :id", [':id' => $id]);
   return FALSE;
 }
 
 
 $feed = Feed::load($id);
 if (!$feed instanceof FeedInterface) {
-  $message = "Called feed import script with numeric feed id number that does not exist: $id";
-  \Drupal::logger('my_module')->error($message);
+  $this->logger->notice('Sent email to %recipient', ['%recipient' => $recipient]);
+  \Drupal::logger('feed_import.php')->error("Called feed import script with numeric feed id number that does not exist: :id", [':id' => $id]);
   return FALSE;
 } else {
   /** @var \Drupal\feeds\Entity\Feed $feed */
-  $feed->unlock();
-  $feed->startCronImport();
+  if (!$feed->isLocked()) {
+    $feed->startCronImport();
+  } else {
+    \Drupal::logger('feed_import.php')->warning("Called feed import script for id number :id, but the feed is locked",[':id' => $id]);
+  }
 }
 
