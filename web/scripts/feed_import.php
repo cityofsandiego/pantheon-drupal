d6dc400e96ec01051f0aab8b62b1e9ec46857b47
Glen Shewchuck <glenshewchuck@gmail.com>
2024-01-30 08:52:38 -0800

    add more logging to feed_import.php

--- web/scripts/feed_import.php
+++ web/scripts/feed_import.php
@@ -7,6 +7,7 @@
 use Drupal\feeds\Entity\Feed;
 use Drupal\feeds\FeedInterface;
 
+// The first argument is the feed id.
 if (empty($extra[0])) {
   \Drupal::logger('feed_import.php')->error("Called feed import script with no feed id number");
   return FALSE;
@@ -14,9 +15,17 @@ if (empty($extra[0])) {
   $id = $extra[0];
 }
 
+// The first argument is the feed id and should be numeric.
 if (!is_numeric($id)) {
   \Drupal::logger('feed_import.php')->error( "Called feed import script with a non numeric feed id number of :id", [':id' => $id]);
   return FALSE;
+}
+
+// See if the cron schedule was passed for logging purposes.
+if (!empty($extra[1])) {
+  $schedule = $extra[1];
+} else {
+  $schedule = 'not given';
 }
 
 
@@ -28,11 +37,11 @@ if (!$feed instanceof FeedInterface) {
 } else {
   /** @var \Drupal\feeds\Entity\Feed $feed */
   if (!$feed->isLocked()) {
-    \Drupal::logger('feed_import.php')->info("About to start feed # :id",[':id' => $id]);
+    \Drupal::logger('feed_import.php')->info("About to start feed # :id (:schedule)",[':id' => $id, ':schedule' => $schedule]);
     $feed->startCronImport();
-    \Drupal::logger('feed_import.php')->info("Started feed # :id",[':id' => $id]);
+    \Drupal::logger('feed_import.php')->info("Started feed # :id (:schedule)",[':id' => $id, ':schedule' => $schedule]);
   } else {
-    \Drupal::logger('feed_import.php')->warning("Called feed import script for id number :id, but the feed is locked",[':id' => $id]);
+    \Drupal::logger('feed_import.php')->warning("Called feed import script for id number :id, but the feed is locked (:schedule)",[':id' => $id, ':schedule' => $schedule]);
   }
 }
 
