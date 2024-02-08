b40629729657f4319202323dbd861bac0b64f268
Glen Shewchuck <glenshewchuck@gmail.com>
2024-02-08 14:38:02 -0800

    sand_feeds module to help with separate queues for each sand_remote feed

---
+++ web/modules/custom/sand_feeds/src/Plugin/QueueWorker/SandFeedRefresh37.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\sand_feeds\Plugin\QueueWorker;
+
+use Drupal\Core\Annotation\QueueWorker;
+use Drupal\feeds\Plugin\QueueWorker\FeedRefresh;
+use Drupal\sand_feeds\SandFeedsQueueExecutable;
+
+/**
+ * A queue worker for importing feeds.
+ *
+ * @QueueWorker(
+ *   id = "sand_feeds_feed_refresh_id:37",
+ *   title = @Translation("Sand Feed Refresh ID:37 - Doc scs_ccagenda_sglu"),
+ *   cron = {"time" = 321},
+ * )
+ */
+class SandFeedRefresh37 extends FeedRefresh {
+
+  /**
+   * Returns Feeds executable.
+   *
+   * @return \Drupal\feeds\FeedsExecutableInterface
+   *   A feeds executable.
+   */
+  protected function getExecutable() {
+    return \Drupal::service('class_resolver')->getInstanceFromDefinition(SandFeedsQueueExecutable::class);
+  }
+
+}
+
---
+++ web/modules/custom/sand_feeds/src/Plugin/QueueWorker/SandFeedRefresh54.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\sand_feeds\Plugin\QueueWorker;
+
+use Drupal\Core\Annotation\QueueWorker;
+use Drupal\feeds\Plugin\QueueWorker\FeedRefresh;
+use Drupal\sand_feeds\SandFeedsQueueExecutable;
+
+/**
+ * A queue worker for importing feeds.
+ *
+ * @QueueWorker(
+ *   id = "sand_feeds_feed_refresh_id:54",
+ *   title = @Translation("Sand Feed Refresh ID:54 - Doc scs_redevelagency_reports"),
+ *   cron = {"time" = 321},
+ * )
+ */
+class SandFeedRefresh54 extends FeedRefresh {
+
+  /**
+   * Returns Feeds executable.
+   *
+   * @return \Drupal\feeds\FeedsExecutableInterface
+   *   A feeds executable.
+   */
+  protected function getExecutable() {
+    return \Drupal::service('class_resolver')->getInstanceFromDefinition(SandFeedsQueueExecutable::class);
+  }
+
+}
+
