fc08d37628283322d6c3eafdd5ab2427cce1aa61
Glen Shewchuck <glenshewchuck@gmail.com>
2024-02-20 09:25:56 -0800

    update sand_media file check action to include output filename

--- web/modules/custom/sand_media/src/Plugin/Action/sandMediaCheckAction.php
+++ web/modules/custom/sand_media/src/Plugin/Action/sandMediaCheckAction.php
@@ -2,9 +2,13 @@
 
 namespace Drupal\sand_media\Plugin\Action;
 
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
 use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
+use Drupal\views_bulk_operations\Action\ViewsBulkOperationsPreconfigurationInterface;
 
 /**
  * This logs any files that are missing from media objects.
@@ -17,19 +21,31 @@ use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
  *   api_version = "1",
  * )
  */
-class sandMediaCheckAction extends ViewsBulkOperationsActionBase {
+class sandMediaCheckAction  extends ViewsBulkOperationsActionBase implements ViewsBulkOperationsPreconfigurationInterface, PluginFormInterface {
 
-  private $filename = 'public://missing_files.txt';
+  private $filename;
 
   private function createFile() {
 
+    $this->filename = pathinfo( $this->configuration['sand_media_check_filename'], PATHINFO_FILENAME) . '.csv';
     if (!file_exists($this->filename)) {
       $file = File::create([
         'filename' => basename($this->filename),
         'uri' => $this->filename,
+        'uid' => \Drupal::currentUser()->id(),
         'status' => 1,
       ]);
       $file->save();
+
+      $media = Media::create([
+        'name' => $file->label(),
+        'bundle' => 'document',
+        'uid' => \Drupal::currentUser()->id(),
+        'field_media_document' => [
+          'target_id' => $file->id(),
+        ],
+      ]);
+      $media->save();
     }
   }
 
@@ -54,6 +70,60 @@ class sandMediaCheckAction extends ViewsBulkOperationsActionBase {
       }
     }
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPreConfigurationForm(array $form, array $values, FormStateInterface $form_state): array {
+    $form['sand_media_check_filename'] = [
+      '#title' => $this->t('File name to write out results to'),
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#default_value' => $values['sand_media_check_filename'] ?? '',
+    ];
+    return $form;
+  }
+
+  /**
+   * Configuration form builder.
+   *
+   * Allow naming of output file.
+   *
+   * @param array $form
+   *   Form array.
+   * @param \Drupal\views_bulk_operations_example\Plugin\Action\Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   *
+   * @return array
+   *   The configuration form.
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form['sand_media_check_filename'] = [
+      '#title' => $this->t('File name WITHOUT extension for results of check'),
+      '#description' => $this->t('Files that are not found will be listed here along with their media id.'),
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#default_value' => $form_state->getValue('sand_media_check_filename'),
+    ];
+    return $form;
+  }
+
+  /**
+   * Submit handler for the action configuration form.
+   *
+   * If not implemented, the cleaned form values will be
+   * passed directly to the action $configuration parameter.
+   *
+   * @param array $form
+   *   Form array.
+   * @param \Drupal\views_bulk_operations_example\Plugin\Action\Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state): void {
+    $this->configuration['sand_media_check_filename'] = $form_state->getValue('sand_media_check_filename');
+  }
+
+
 
   /**
    * {@inheritdoc}
